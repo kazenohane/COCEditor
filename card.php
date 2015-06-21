8774522425b230bc5f70ca17ce85449db75ad45f
Hang <191743150@qq.com>
2015-06-21 18:02:16 +0800

    1. now set card-gallery to be open in new page   in index.html 2. Players can update their investigator cards' background and info using password

--- card.php
+++ card.php
@@ -318,31 +318,49 @@ $specialSkillID = array(57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
     <div id="div_update_image">
         <form id="submit_image" method="post" action="uploadImage.php<?php echo('?cardid='.$card["cID"].'&player='.$card["cPlayer"]);?>" enctype="multipart/form-data" target="uploadfile">
 
-          <div class="form-group div_update_align">
-          
+          <div class="form-group">
+          <div class="div_update_align">
             <label>修改调查员形象</label>
-           
+           </div>
             <input type="file" name ="image" id="input_image_file" />
             <p class="help-block">仅接受1MB以内的jpg与png格式图像文件</p>
           </div>
           
           <div class="div_update_align">
-          <button type="submit" id="button_image_submit" class="btn btn-success">提交</button>
+          <button type="submit" id="button_image_submit" class="btn btn-success">提交形象</button>
           </div>
         </form>
         <iframe name="uploadfile" width="0px" height="0px"></iframe>
+     </div>  
+     <div id="div_update_info">   
         
-        
-        <div class="div_update_align">
-        <label>修改调查员资料</label>
+
+        <div class="row">
+		    <label>修改调查员物品</label>
         </div>
-		<textarea id="input_background" class="input_info"  placeholder ="人物背景"></textarea>
-		
 		<textarea id="input_item" class="input_info" placeholder ="携带物品"></textarea>
-        <div class="div_update_align">
-		<button type="submit" id="button_info_submit" class="btn btn-success">提交</button>
-        <br><br>
+        
+        <div class="row">
+            <label>修改调查员背景</label>
+            </div>
+        
+		<textarea id="input_background" class="input_info"  placeholder ="人物背景"></textarea>
+        
+        <div class="row">
+ 
+            <div class="col-xs-12 col-sm-12 col-md-3">
+		    <input type="password"  class="form-control input_info" placeholder = "玩家密码" id="input_password"></input>            
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3">
+            <button type="submit" id="button_info_submit" class="btn btn-success">提交信息</button>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3">
+                <div id="div_update_result">
+                    *密码设置请联系管理员
+                </div>
+            </div>
         </div>
+
     </div>
     <div class="div_card_end">
     <br>
@@ -354,6 +372,10 @@ $specialSkillID = array(57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
 </body>
 
 <script type="text/javascript">
+    function showResult(data){
+        $("#div_update_result").html(data);
+        
+    }
     $(document).ready(function () {
 
         $(".div_card_image").css("background-image", "url(<?php echo($card['cImage']);?> )");
@@ -397,14 +419,23 @@ $specialSkillID = array(57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
                 valid = true;
             }
             
+            var password = $("#input_password").val();
+            jsonObj["pwd"] = password ;
+            
+            jsonObj["cardid"]= <?php echo($card["cID"]);?>;
+            jsonObj["player"]= "<?php echo($card["cPlayer"]);?>";
+             
             if(valid == true){    
-                var targetURL = "updateInfo";
+                var targetURL = "updateInfo.php";
                 var json = JSON.stringify(jsonObj);
-                alert(json);
+                
+                $("#div_update_result").html("请求提交中......");
                 $.post(targetURL, { info: json }, function (data) {
                     showResult(data);
                 });   
     
+            }else{
+                $("#div_update_result").html("输入数据为空");
             }
         });
     
---
+++ updateInfo.php
@@ -0,0 +1,137 @@
+﻿<?php
+
+/**
+ * uploadImage 
+ *
+ * 
+ *
+ * @version 1.0
+ * @author Fengyu
+ */
+include 'common.php';
+
+function validatePassord($p,$q){
+
+    if($q ==null){
+        $q = "callofcthulhu";
+    }
+    if($p == $q){
+        return true;
+    }
+    return false;
+}
+function validateAndUpdate($cardID,$player,$background,$item,$password){
+    
+    $cID = (int)$cardID;
+    $cPlayer = $player;
+    
+    
+    //Connect
+    $mysqli = new mysqli("153.120.6.104", "coc", "fengyu", "db_coc");
+    if ($mysqli->connect_errno) {
+        echo "Connect Failure";
+    }
+    $cPlayer = sqlFilter($mysqli,$player,SQL_STRING);
+
+    
+    $sql_get = "SELECT * FROM Players WHERE cName = ?" ;
+    if ($stmt =$mysqli->prepare($sql_get)){
+        
+        /* bind parameters for markers */
+        $stmt->bind_param('s',$cPlayer);
+
+        
+        $res = $stmt->execute();
+        
+        /* fetch value */
+        $result = $stmt->get_result();
+        $card = $result->fetch_array();
+        
+        $stmt->close();
+        
+        if($card == null){
+            if(!validatePassord($password,null)){       
+                $mysqli->close();
+                return false;
+            }
+        }else{
+        
+            $storedPassword = $card['cPassword'];
+             if(!validatePassord($password,$storedPassword)){
+                $mysqli->close();
+                return false;
+            }
+        }
+        
+    }
+    
+    
+    $sql_get = "SELECT * FROM Investigators WHERE cID = ? AND cPlayer = ?" ;
+    
+    /* create a prepared statement */
+    if ($stmt =$mysqli->prepare($sql_get)){
+        
+        /* bind parameters for markers */
+        $stmt->bind_param('is',$cID,$cPlayer);
+
+        $cID = sqlFilter($mysqli,$cID,SQL_INT);   
+        $cPlayer = sqlFilter($mysqli,$player,SQL_STRING);
+        
+        $res = $stmt->execute();
+        
+        /* fetch value */
+        $result = $stmt->get_result();
+        $card = $result->fetch_array();
+        
+        $stmt->close();
+        if($card == null){
+            $mysqli->close();
+            return false;
+        }
+        if($background != null && $background != ''){
+            $sql_update = "UPDATE Investigators SET cBackground = ? WHERE cID = ?";
+            if ($stmt =$mysqli->prepare($sql_update)){
+                /* bind parameters for markers */
+                $stmt->bind_param('si',$background,$cID);
+
+                $res = $stmt->execute();
+                if(!$res){
+                    $mysqli->close();
+                    return false;
+                }
+            }
+        }
+        if($item != null && $item != ''){
+            $sql_update = "UPDATE Investigators SET cItem = ? WHERE cID = ?";
+            if ($stmt =$mysqli->prepare($sql_update)){
+                /* bind parameters for markers */
+                $stmt->bind_param('si',$item,$cID);
+
+                $res = $stmt->execute();
+                if(!$res){
+                    $mysqli->close();
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+
+$str = $_POST["info"];
+$obj = json_decode($str,true);
+$cardID = $obj["cardid"];
+$player = $obj["player"];
+$background = $obj["bg"];
+$item = $obj["item"];
+$password = $obj["pwd"];
+
+if(!validateAndUpdate($cardID,$player,$background,$item,$password)){
+    echo("玩家 [".$player. "] 密码验证错误");
+}else{
+    echo("资料更新成功，请刷新页面");
+}
+
+
+?>
